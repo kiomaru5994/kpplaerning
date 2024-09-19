7baa3d6f90d153b30be26a0f996a75c367b9e8bf
kiomaru <alias5994@yandex.ru>
2024-09-19 17:18:02 +0300

    1.1.4 task Add Hibernate realization

--- src/main/java/ru/kiomaru/ppproject/model/User.java
+++ src/main/java/ru/kiomaru/ppproject/model/User.java
@@ -1,21 +1,22 @@
 package ru.kiomaru.ppproject.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Entity
+@Table(name = "Users")
 public class User {
     @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "lastName")
     private String lastName;
 
-    @Column
+    @Column(name = "age")
     private Byte age;
 
     public User() {
@@ -60,7 +61,7 @@ public class User {
         this.age = age;
     }
 
-    public String toString(){
+    public String toString() {
         return "ID: " + getId() + " " + "| Name: " + getName() + " " + "| LastName: " + getLastName() + " " + "| Age: " + getAge();
     }
 }
